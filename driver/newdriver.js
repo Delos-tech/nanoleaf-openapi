361b9cde964ea182fa7066898f78e209e9dace01
Joseph Ottinger <joeo@enigmastation.com>
2022-07-29 09:11:16 -0400

    fixing license check, exposing color brightness to device

--- driver/newdriver.js
+++ driver/newdriver.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 const { Discovery } = require('../lib/index');
 
 async function main() {
@@ -6,11 +7,14 @@ async function main() {
     discovery.on('new-device', async (e) => {
         console.log(e);
         e.setAuthToken('11tnYUmDjr92hQssdrA16dXNqKXObEPE');
+        e.allowColorBrightness = false;
         console.log(await e.getState());
-        await e.identify();
+        await e.set({ color: 'blue' });
     });
     discovery.start();
 }
 
 // eslint-disable-next-line more/no-then
-main().then();
+main().then(() => {
+    setTimeout(() => process.exit(0), 15000);
+});
